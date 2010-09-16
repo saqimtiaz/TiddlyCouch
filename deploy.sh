800fb19d884ae6db80339b24aaaa943b32e2ec59
FND <fnd@fnd-oda.(none)>
2010-09-16 16:55:27 +0200

    added ServerSideSavingPlugin
    required a fugly hack in js2tiddler...

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e
 
 url="$*"
 
+curl -s "http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js" | \
+	./js2tiddler > fixtures/ServerSideSavingPlugin.json
+
 ./js2tiddler plugins/adaptor.js > fixtures/adaptor.json
 couchapp pushdocs fixtures $url
 
