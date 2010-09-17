d23adee08b6c8eb256fa2a841b6c9d44a78a9468
Saq <saq@saq-vm-ub910.(none)>
2010-09-17 09:06:45 +0200

    Added CouchdbConfig to allow saving of new tiddlers

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ url="$*"
 	> fixtures/ServerSideSavingPlugin.json
 ./js2tiddler "http://jquery-json.googlecode.com/files/jquery.json-2.2.min.js" \
 	> fixtures/jquery-json.json
-./js2tiddler plugins/adaptor.js > fixtures/adaptor.json
+./js2tiddler plugins/CouchdbAdaptor.js > fixtures/CouchdbAdaptor.json
+./js2tiddler plugins/CouchdbConfig.js > fixtures/CouchdbConfig.json
 
 couchapp pushdocs fixtures $url
 
